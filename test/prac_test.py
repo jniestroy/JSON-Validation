a4349e66e932a75ebfc38e2ef7d4f215d5c23348
Justin Niestroy <jniestroy@gmail.com>
2019-06-24 15:44:13 -0400

    fixed tests

--- test/prac_test.py
+++ test/prac_test.py
@@ -4,13 +4,13 @@ import requests
 class Testflaskapp(unittest.TestCase):
     def test_valid_json(self):
         testjson = {'@context': "http://json-schema.org/draft-07/schema","@type":"Dataset","name":"Justin"}
-        URL = "http://localhost:5000/validatejson"
+        URL = "0.0.0.0:5000/validatejson"
         r = requests.post(url = URL,json = testjson)
         self.assertEqual(r.json(), {'valid':True,'non schema properties':[]})
     
     def test_extra_property(self):
         testjson = {'@context': "http://json-schema.org/draft-07/schema","@type":"Dataset","name":"Justin","randomname":"test"}
-        URL = "http://localhost:5000/validatejson"
+        URL = "0.0.0.0:5000:5000/validatejson"
         r = requests.post(url = URL,json = testjson)
         self.assertEqual(r.json(), {'valid':True,'non schema properties':["randomname"]})
 if __name__ == '__main__':
