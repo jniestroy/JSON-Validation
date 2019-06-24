e1622ac4d9cd94c702be64d80b80ea6f8a883dc0
Justin Niestroy <jniestroy@gmail.com>
2019-06-24 16:35:39 -0400

    updated file structure

--- app/test/prac_test.py
+++ app/test/prac_test.py
@@ -1,7 +1,10 @@
 import unittest
 import requests
+from app import app
 
 class Testflaskapp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.app.test_client()
     def test_valid_json(self):
         testjson = {'@context': "http://json-schema.org/draft-07/schema","@type":"Dataset","name":"Justin"}
         URL = " http://127.0.0.1:5000/validatejson"
