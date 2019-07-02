113514eef76d13854def2857f0849a135ecf9e65
Justin Niestroy <jniestroy@gmail.com>
2019-07-02 15:03:58 -0400

    Fixed Tests

--- app/test/prac_test.py
+++ app/test/prac_test.py
@@ -6,13 +6,100 @@ class Testflaskapp(unittest.TestCase):
     def setUp(self):
         self.app = app.app.test_client()
     def test_valid_json(self):
-        testjson = {'@context': "http://json-schema.org/draft-07/schema","@type":"Dataset","name":"Justin","description":"Test dataset","dataCreated":"5/12/1990","author":{"name":"check","url":"test"}}
+        testjson = {
+  "@context":{ "@vocab": "http://schema.org/" },
+  "@type":"Dataset",
+  "name":"sample",
+  "dateCreated":"19/9/1995",
+  "description":"Storm Data is provided by the National Weather Service (NWS) and contain statistics on...",
+  "url":"https://catalog.data.gov/dataset/ncdc-storm-events-database",
+  "sameAs":"https://gis.ncdc.noaa.gov/geoportal/catalog/search/resource/details.page?id=gov.noaa.ncdc:C00510",
+  "identifier": ["https://doi.org/10.1000/182",
+                 "https://identifiers.org/ark:/12345/fk1234"],
+  "keywords":[
+     "ATMOSPHERE > ATMOSPHERIC PHENOMENA > CYCLONES",
+     "ATMOSPHERE > ATMOSPHERIC PHENOMENA > DROUGHT",
+     "ATMOSPHERE > ATMOSPHERIC PHENOMENA > FOG",
+     "ATMOSPHERE > ATMOSPHERIC PHENOMENA > FREEZE"
+  ],
+  "author":{
+     "@type":"Organization",
+     "url": "https://www.ncei.noaa.gov/",
+     "name":"OC/NOAA/NESDIS/NCEI > National Centers for Environmental Information, NESDIS, NOAA, U.S. Department of Commerce",
+     "contactPoint":{
+        "@type":"ContactPoint",
+        "contactType": "customer service",
+        "telephone":"+1-828-271-4800",
+        "email":"ncei.orders@noaa.gov"
+     }
+  },
+  "includedInDataCatalog":{
+     "@type":"DataCatalog",
+     "name":"data.gov"
+  },
+  "distribution":[
+     {
+        "@type":"DataDownload",
+        "encodingFormat":"CSV",
+        "contentUrl":"http://www.ncdc.noaa.gov/stormevents/ftp.jsp"
+     },
+     {
+        "@type":"DataDownload",
+        "encodingFormat":"XML",
+        "contentUrl":"http://gis.ncdc.noaa.gov/all-records/catalog/search/resource/details.page?id=gov.noaa.ncdc:C00510"
+     }
+  ]
+} 
         URL = " http://127.0.0.1:5000/validatejson"
         r = self.app.post('/validatejson',json = testjson)
         self.assertEqual(r.json, {'error': '', 'extra_elements': [], 'valid': True})
     
     def test_extra_property(self):
-        testjson = {'@context': "http://json-schema.org/draft-07/schema","@type":"Dataset","name":"Justin","description":"Test dataset","dataCreated":"5/12/1990","author":{"name":"check","url":"test"},"random":"test"}
+        testjson = {
+  "@context":{ "@vocab": "http://schema.org/" },
+  "@type":"Dataset",
+  "randomname":"this should fail",
+  "name":"sample",
+  "dateCreated":"19/9/1995",
+  "description":"Storm Data is provided by the National Weather Service (NWS) and contain statistics on...",
+  "url":"https://catalog.data.gov/dataset/ncdc-storm-events-database",
+  "sameAs":"https://gis.ncdc.noaa.gov/geoportal/catalog/search/resource/details.page?id=gov.noaa.ncdc:C00510",
+  "identifier": ["https://doi.org/10.1000/182",
+                 "https://identifiers.org/ark:/12345/fk1234"],
+  "keywords":[
+     "ATMOSPHERE > ATMOSPHERIC PHENOMENA > CYCLONES",
+     "ATMOSPHERE > ATMOSPHERIC PHENOMENA > DROUGHT",
+     "ATMOSPHERE > ATMOSPHERIC PHENOMENA > FOG",
+     "ATMOSPHERE > ATMOSPHERIC PHENOMENA > FREEZE"
+  ],
+  "author":{
+     "@type":"Organization",
+     "url": "https://www.ncei.noaa.gov/",
+     "name":"OC/NOAA/NESDIS/NCEI > National Centers for Environmental Information, NESDIS, NOAA, U.S. Department of Commerce",
+     "contactPoint":{
+        "@type":"ContactPoint",
+        "contactType": "customer service",
+        "telephone":"+1-828-271-4800",
+        "email":"ncei.orders@noaa.gov"
+     }
+  },
+  "includedInDataCatalog":{
+     "@type":"DataCatalog",
+     "name":"data.gov"
+  },
+  "distribution":[
+     {
+        "@type":"DataDownload",
+        "encodingFormat":"CSV",
+        "contentUrl":"http://www.ncdc.noaa.gov/stormevents/ftp.jsp"
+     },
+     {
+        "@type":"DataDownload",
+        "encodingFormat":"XML",
+        "contentUrl":"http://gis.ncdc.noaa.gov/all-records/catalog/search/resource/details.page?id=gov.noaa.ncdc:C00510"
+     }
+  ]
+} 
         URL = " http://127.0.0.1:5000/validatejson"
         r = self.app.post('/validatejson',json = testjson)
         self.assertEqual(r.json, {'error': '', 'extra_elements': ["randomname"], 'valid': True})
