be9f06c370890be3de67817407a1da8edddf4f86
jniestroy <jniestroy@gmail.com>
2019-07-24 11:44:44 -0400

    Read pickle files in for improved speed

--- app/validate.py
+++ app/validate.py
@@ -11,56 +11,62 @@ class RDFSValidator(object):
     #     Set up RDFSValidator class, read in json-ld to validate, open RDFS
     #     definition file and parse into ...
     # '''
-        with open(path + "schema.jsonld", "rb") as file:
-            schema_rdfs = json.loads(file.read())
-        with open(path + "rdfs_bioschemas_definition.jsonld","rb") as file:
-            bio_rdfs = json.loads(file.read())
-
-        self.g = rdflib.Graph().parse(
-            data = json.dumps(bio_rdfs.get("@graph")),
-            context=bio_rdfs.get("@context"), format="json-ld",publicID = "http://bioschemas.org/specifications/").parse(
-            data = json.dumps(schema_rdfs.get("@graph")),
-            context=schema_rdfs.get("@context"), format="json-ld",)
-        #self.g = rdflib.Graph().parse(data = json.dumps(schema_rdfs.get("@graph")),
-        #context=schema_rdfs.get("@context"), format="json-ld")
-
-        classes = [ elem.get("@id") for elem in schema_rdfs['@graph'] if elem.get("@type") == "rdfs:Class" ]
-        properties = [ elem.get("@id") for elem in schema_rdfs['@graph'] if elem.get("@type") == "rdf:Property" ]
-        classes.extend([ elem.get("@id") for elem in bio_rdfs['@graph'] if elem.get("@type") == "rdfs:Class" ])
-        properties.extend([ elem.get("@id") for elem in bio_rdfs['@graph'] if elem.get("@type") == "rdf:Property" ])
-
-        #self.schema = { elem.get("@id"): elem for elem in schema_rdfs}
-        #For now assuming schema is schema.org can exapnd to accept more later
         self.context = "http://schema.org/"
         self.error = ""
         self.extra_elements = []
         self.data = data
 
+        try:
+            self.schema_properties = pickle.load( open(path +  "schema_properties.p", "rb" ) )
+            self.schema_property_ranges = pickle.load( open(path +  "schema_property_ranges.p", "rb" ) )
+        except:
+            with open(path + "schema.jsonld", "rb") as file:
+                schema_rdfs = json.loads(file.read())
+            with open(path + "rdfs_bioschemas_definition.jsonld","rb") as file:
+                bio_rdfs = json.loads(file.read())
 
-        #Creates list of all properites for each class
-        #including those which are inherited from classes above
-        self.schema_properties = {}
-        for clas in classes:
-            self.schema_properties[clas] = []
+            self.g = rdflib.Graph().parse(
+                data = json.dumps(bio_rdfs.get("@graph")),
+                context=bio_rdfs.get("@context"), format="json-ld",publicID = "http://bioschemas.org/specifications/").parse(
+                data = json.dumps(schema_rdfs.get("@graph")),
+                context=schema_rdfs.get("@context"), format="json-ld",)
+            #self.g = rdflib.Graph().parse(data = json.dumps(schema_rdfs.get("@graph")),
+            #context=schema_rdfs.get("@context"), format="json-ld")
 
-            superClasses = [f for f in self.g.transitive_objects(
-                rdflib.term.URIRef(clas),
-                rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'))]
+            classes = [ elem.get("@id") for elem in schema_rdfs['@graph'] if elem.get("@type") == "rdfs:Class" ]
+            properties = [ elem.get("@id") for elem in schema_rdfs['@graph'] if elem.get("@type") == "rdf:Property" ]
+            classes.extend([ elem.get("@id") for elem in bio_rdfs['@graph'] if elem.get("@type") == "rdfs:Class" ])
+            properties.extend([ elem.get("@id") for elem in bio_rdfs['@graph'] if elem.get("@type") == "rdf:Property" ])
 
-            for superClass in superClasses:
+            #self.schema = { elem.get("@id"): elem for elem in schema_rdfs}
+            #For now assuming schema is schema.org can exapnd to accept more later
 
-                self.schema_properties[clas].extend([str(found)
-                    for found in self.g.transitive_subjects(
-                            rdflib.term.URIRef("http://schema.org/domainIncludes"),
-                            rdflib.term.URIRef(superClass))])
 
-        #Gathers acceptable ranges for all properties found
-        self.schema_property_ranges = {}
-        for prop in properties:
-            self.schema_property_ranges[prop] = [str(f)
-                for f in self.g.transitive_objects(
-                        rdflib.term.URIRef(prop),
-                        rdflib.term.URIRef("http://schema.org/rangeIncludes"))]
+
+            #Creates list of all properites for each class
+            #including those which are inherited from classes above
+            self.schema_properties = {}
+            for clas in classes:
+                self.schema_properties[clas] = []
+
+                superClasses = [f for f in self.g.transitive_objects(
+                    rdflib.term.URIRef(clas),
+                    rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'))]
+
+                for superClass in superClasses:
+
+                    self.schema_properties[clas].extend([str(found)
+                        for found in self.g.transitive_subjects(
+                                rdflib.term.URIRef("http://schema.org/domainIncludes"),
+                                rdflib.term.URIRef(superClass))])
+
+            #Gathers acceptable ranges for all properties found
+            self.schema_property_ranges = {}
+            for prop in properties:
+                self.schema_property_ranges[prop] = [str(f)
+                    for f in self.g.transitive_objects(
+                            rdflib.term.URIRef(prop),
+                            rdflib.term.URIRef("http://schema.org/rangeIncludes"))]
 
     #Validates given json-ld
     def validate(self):
